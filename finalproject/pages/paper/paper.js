2e5749fb13e46a87fbc2f89d880f9ef83b5f8282
zaniwilson <zani.wilson@nyu.edu>
2024-12-10 18:52:59 -0500

    Add files via upload

---
+++ finalproject/pages/paper/paper.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const returnButton = document.getElementById('returnButton');
+    returnButton.addEventListener('click', () => {
+        window.location.href = '/index.html';
+    });
+});
