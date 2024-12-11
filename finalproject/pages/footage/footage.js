044d6a74083dedbcbd6e7ec18fef1517bea8d002
zaniwilson <zani.wilson@nyu.edu>
2024-12-10 19:20:56 -0500

    Delete finalproject directory

--- finalproject/pages/footage/footage.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    const returnButton = document.getElementById('returnButton');
-    returnButton.addEventListener('click', () => {
-        window.location.href = '/index.html';
-    });
-});
